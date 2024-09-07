6d9add1772e58027e2c1aa07a41eed97c3066d10
Yunus UÇAK <yunus.ucak@outlook.com>
2024-09-07 19:52:32 +0300

    maybe images fix

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -119,7 +119,7 @@ export default function Album() {
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image="https://source.unsplash.com/photos/random"
                     title="Image title"
                   />
                   <CardContent className={classes.cardContent}>
