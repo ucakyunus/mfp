e0d52bc290d0a925a61212cd3510c5f448b7c5f3
Yunus UÇAK <yunus.ucak@outlook.com>
2024-09-07 19:24:52 +0300

    added invalidation step to build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
 	return (
 		<div>
-			<h1>Container</h1>
+			<h1>Container App</h1>
 			<hr />
 			<MarketingApp />
 		</div>
